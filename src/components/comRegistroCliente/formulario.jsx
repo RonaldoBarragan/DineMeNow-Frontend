a158647420c83539265b237380e75e6c8b9a262e
karen rodriguez <monteroluz979@gmail.com>
2025-11-25 19:34:12 -0500

    Actualizacion main-rama

---
+++ src/components/comRegistroCliente/formulario.jsx
@@ -0,0 +1,36 @@
+import Form from 'react-bootstrap/Form';
+function Formregristrousu () {
+    return (
+        <>
+        <h4>Crea tu cuenta</h4>
+        <Form >
+            <Form.Group>
+                <Form.Label>Nombre Completo</Form.Label>
+                <Form.Control type="text" placeholder="Tu Nombre Completo">
+                </Form.Control>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Email</Form.Label>
+                <Form.Control type="email" placeholder="Ingresa tu carreo">
+                </Form.Control>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Telefono</Form.Label>
+                <Form.Control type="number" placeholder="3001234567">
+                </Form.Control>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Contraseña</Form.Label>
+                <Form.Control type="password" >
+                </Form.Control>
+            </Form.Group>
+            <Form.Group>
+                <Form.Label>Confirmar Contraseña</Form.Label>
+                <Form.Control type="password" >
+                </Form.Control>
+            </Form.Group>
+        </Form>
+        </>
+    )
+}
+export default Formregristrousu
