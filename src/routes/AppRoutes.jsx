4d7c0c55695cca9bddba8d2072d706239e0ff454
Diego <166516606+Diego0917@users.noreply.github.com>
2025-11-26 20:17:24 -0500

    Recuperar contraseña
    Componente recuperar contraseña, creación estilo recuperar css y ruta.

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,8 +1,8 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import IniciarSesion from '../pages/IniciarSesion';
-import VerificarToken from '../pages/VerificarToken';
-import Home from '../pages/Home';
-
+import { Routes, Route, Navigate } from "react-router-dom";
+import IniciarSesion from "../pages/IniciarSesion";
+import VerificarToken from "../pages/VerificarToken";
+import Recuperar from "../components/comRecuperarContraseña/Contraseña";
+import Home from "../pages/Home";
 
 export const AppRoutes = () => {
   return (
@@ -10,9 +10,10 @@ export const AppRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="/iniciarSesion" element={<IniciarSesion />} />
       <Route path="/verificarToken" element={<VerificarToken />} />
+      <Route path="/recuperar" element={<Recuperar />} />
 
       {/* Redirección por defecto */}
       <Route path="/*" element={<Navigate to="/" />} />
     </Routes>
-  )
-}
+  );
+};
