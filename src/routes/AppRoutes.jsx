96f7ec7ef073c9880a2679451d20cd19fcba8003
Leidy Estupiñan <leidyestupinan37@gmail.com>
2025-12-02 19:12:55 -0500

    Arreglo rutas

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,34 +1,17 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import IniciarSesion from '../pages/IniciarSesion';
-import Registro_Restaurante from '../pages/Registro-Restaurantes';
+import VerificarToken from '../pages/VerificarToken';
 import Home from '../pages/Home';
-
-
-
-
-// ------------------------------------------------------------
-// ZONA DE IMPORTACIONES (Descomentar cuando existan los archivos)
-// ------------------------------------------------------------
-// import Home from '../pages/Home';
-// import Login from '../pages/Login';
-
-
-// ------------------------------------------------------------
-// COMPONENTES DUMMY (Para que no de error mientras crean las paginas)
-// Eliminar esto cuando tus compañeros creen los archivos reales
-// ------------------------------------------------------------
-const HomeDummy = () => <h2 className="text-center mt-5">🏡 Página Home (Pendiente)</h2>;
-//const LoginDummy = () => <h2 className="text-center mt-5">🔐 Página Login (Pendiente)</h2>;
+import Registro_Restaurante from '../pages/Registro-Restaurantes';
 
 
 export const AppRoutes = () => {
   return (
     <Routes>
-      {/* Rutas usando los Dummys */}
-      <Route path="/" element={<HomeDummy />} />
-      <Route path="/IniciarSesion" element={<IniciarSesion />} />
-      <Route path="/Registro" element={<Registro_Restaurante />} />
-      <Route path="/jeimy" element={<Home />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/iniciarSesion" element={<IniciarSesion />} />
+      <Route path="/verificarToken" element={<VerificarToken />} />
+      <Route path="/registro-restaurantes" element={<Registro_Restaurante />} />
 
       {/* Redirección por defecto */}
       <Route path="/*" element={<Navigate to="/" />} />
