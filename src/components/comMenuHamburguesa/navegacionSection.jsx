2d0a0e1aaf6b26bfec1c8dee7540b84fd8e6e424
RonaldoBarragan <barraganronaldo1@gmail.com>
2025-12-04 17:39:40 -0500

    Add dynamic navigation based on route
    NavigationSection now displays different menu items depending on the current route, supporting public, client, admin, and restaurant views. This improves user experience by showing relevant navigation options for each user type.

--- src/components/comMenuHamburguesa/navegacionSection.jsx
+++ src/components/comMenuHamburguesa/navegacionSection.jsx
@@ -2,13 +2,32 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { BiHomeAlt } from "react-icons/bi"; 
 import "./navegacionSection.css";
+import { useLocation } from "react-router-dom";
 
 function NavigationSection() {
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  const publicRoutes = ["/", "/registro-restaurantes", "/acerca-de"];
+
+  const clientRoutes = ["/vista-cliente", "/reservas", "/favoritos"];
+
+  const isPublicView = publicRoutes.includes(currentPath);
+  const isClientView = clientRoutes.includes(currentPath);
+
+
+   // Menú de Administrador (Ruta: '/vista-administrador')
+  const isAdminView = currentPath === "/vista-administrador";
+  // Menú de Restaurante (Nueva Ruta: '/vista-restaurante')
+  const isRestaurantView = currentPath === "/vista-restaurante";
+
+
   return (
     <nav className="menu-navigation">
       <h3 className="nav-title">NAVEGACIÓN</h3>
 
       <ul>
+        {isPublicView && (
         <li>
           <NavLink to="/" className="nav-item">
             <BiHomeAlt size={20} className="nav-icon" />
@@ -16,13 +35,32 @@ function NavigationSection() {
           </NavLink>
           
         </li>
-        {/* <li>
+        )}
+
+        {isClientView && (<>
+        <li>
           <NavLink to="/" className="nav-item">
             <BiHomeAlt size={20} className="nav-icon" />
             <span>Buscar Restaurantes</span>
           </NavLink>
           
-        </li> */}
+        </li>
+        <li>
+          <NavLink to="/" className="nav-item">
+            <BiHomeAlt size={20} className="nav-icon" />
+            <span>Buscar Restaurantes</span>
+          </NavLink>
+          
+        </li>
+        <li>
+          <NavLink to="/" className="nav-item">
+            <BiHomeAlt size={20} className="nav-icon" />
+            <span>Buscar Restaurantes</span>
+          </NavLink>
+          
+        </li>
+        </>)}
+
       </ul>
 
       <hr className="nav-divider" />
