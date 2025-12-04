06503c2f5a2317477d518325c26ba574f0340a64
Karen <monteroluz979@gmail.com>
2025-12-03 21:11:33 -0500

    Estilos

--- src/components/comRegistroCliente/formulario.jsx
+++ src/components/comRegistroCliente/formulario.jsx
@@ -1,38 +1,64 @@
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import './Formregistrocliente.css';
+import { AiOutlineUser } from "react-icons/ai";
+import { BsEnvelope } from "react-icons/bs";
+import { FiPhone } from "react-icons/fi";
+import { AiOutlineLock } from "react-icons/ai";
 function Formregristrousu () {
     return (
         <>
         <div className="form-registro">
-        <p>Crear cuenta</p>
+        <p>Crear Cuenta</p>
         <Form >
             <Form.Group>
                 <Form.Label>Nombre Completo</Form.Label>
-                <Form.Control type="text" placeholder="Tu Nombre Completo">
-                </Form.Control>
+                    <div className='input-container-relative'>
+                    <Form.Control type="text" placeholder="Tu Nombre Completo" className='inputForm icon-form-padding-left'/>
+                        <div className='icon-form-overlay'>
+                    <AiOutlineUser size={15} />
+                        </div>
+                    </div>
             </Form.Group>
             <Form.Group>
                 <Form.Label>Email</Form.Label>
-                <Form.Control type="email" placeholder="Ingresa tu carreo">
-                </Form.Control>
+                    <div className='input-container-relative'>
+                    <Form.Control type="email" placeholder="Ingresa tu carreo" className='inputForm icon-form-padding-left'/>
+                        <div className='icon-form-overlay'>
+                    <BsEnvelope size={15} />
+                        </div>
+                    </div>
             </Form.Group>
             <Form.Group>
                 <Form.Label>Telefono</Form.Label>
-                <Form.Control type="tel" placeholder="3001234567">
-                </Form.Control>
+                    <div className='input-container-relative'>
+                    <Form.Control type="tel" placeholder="3001234567" className='inputForm icon-form-padding-left'/>
+                        <div className='icon-form-overlay'>
+                    <FiPhone size={15} />
+                        </div>
+                    </div>
             </Form.Group>
             <Form.Group>
                 <Form.Label>Contraseña</Form.Label>
-                <Form.Control type="password" >
-                </Form.Control>
+                    <div className='input-container-relative'>
+                    <Form.Control type="password" placeholder="••••••••"  className='inputForm icon-form-padding-left'/>
+                        <div className='icon-form-overlay'>
+                    <AiOutlineLock size={15} />
+                        </div>
+                    </div>
             </Form.Group>
             <Form.Group>
                 <Form.Label>Confirmar Contraseña</Form.Label>
-                <Form.Control type="password" >
-                </Form.Control>
+                    <div className='input-container-relative'>
+                    <Form.Control type="password" placeholder="••••••••"  className='inputForm icon-form-padding-left'/>
+                        <div className='icon-form-overlay'>
+                    <AiOutlineLock size={15} />
+                        </div>
+                    </div>
             </Form.Group>
-            <Button type='submit'>Crear Cuenta</Button>
+            <Button  type="submit" size="sm" className="buttonNaranjaDegrade w-100">
+            Crear Cuenta
+            </Button>
         </Form>
         </div>
         </>
