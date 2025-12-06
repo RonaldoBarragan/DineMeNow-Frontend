5c8b0ed11ba13212ab5ec9b9fb3fb6314ed40270
Ronaldo Barragan <barraganronaldo1@gmail.com>
2025-12-05 20:34:46 -0500

    Wrap GestionPerfilCliente in Bootstrap Container
    Added react-bootstrap Container to GestionPerfilCliente for improved layout and styling. Applied padding and custom container class for consistent UI.

--- src/pages/GestionPerfilCliente.jsx
+++ src/pages/GestionPerfilCliente.jsx
@@ -1,8 +1,11 @@
+import { Container } from "react-bootstrap"
 
 export default function GestionPerfilCliente() {
     return (
+        <Container className="container py-5 container-cards">
         <div>
             <h1>Gestión de Perfil del Cliente</h1>
         </div>
+        </Container>
     )
 }
