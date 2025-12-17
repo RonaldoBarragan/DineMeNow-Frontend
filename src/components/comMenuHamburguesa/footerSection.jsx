11db11b92612556f3b142627013ef9794ef2324e
RonaldoBarragan <barraganronaldo1@gmail.com>
2025-12-16 22:22:20 -0500

    Update navigation routes for consistency
    Rutas de ruta estandarizadas en los componentes Footer, Header, SessionRequiredModal y footerSection para usar minúsculas y convenciones de nomenclatura consistentes. Se actualizaron los controladores de botones para que coincidan con la nueva estructura de rutas y mejorar la fiabilidad de la navegación.

--- src/components/comMenuHamburguesa/footerSection.jsx
+++ src/components/comMenuHamburguesa/footerSection.jsx
@@ -3,6 +3,8 @@ import { BiLogIn, BiLogOut } from "react-icons/bi";
 import "./footerSection.css";
 import { Button } from "react-bootstrap";
 import { useLocation } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
 function FooterSection() {
   const location = useLocation();
   const currentPath = location.pathname;
@@ -12,13 +14,14 @@ function FooterSection() {
 
   const isPublicView = publicRoutes.includes(currentPath);
   const isPrivateView = privateRoutes.includes(currentPath);
-  
+  const navigate = useNavigate();
   return (
     <>
     {isPublicView && (
       <div className="menu-session-cta">
         <p>Inicia sesión para acceder a todas las funciones</p>
-        <Button className="btn-login-menu">
+        <Button className="btn-login-menu"
+        onClick={() => navigate("/iniciarsesion")}>
           <BiLogIn size={18} className="login-icon" />
           Iniciar Sesión
         </Button>
@@ -27,7 +30,8 @@ function FooterSection() {
 
     {isPrivateView && (
       <div className="menu-session-cta">
-        <Button className="buttonCerrarSesion">
+        <Button className="buttonCerrarSesion"
+        onClick={() => navigate("/")}>
           <BiLogOut  size={18} className="login-icon" />
           Cerrar Sesión
         </Button>
