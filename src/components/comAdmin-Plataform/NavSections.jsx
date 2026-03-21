c7ff37089a2714ed08f8ca9b40a17a4726d54895
Juan <juancardenas083@gmail.com>
2026-03-20 20:33:07 -0500

    Act content

--- src/components/comAdmin-Plataform/NavSections.jsx
+++ src/components/comAdmin-Plataform/NavSections.jsx
@@ -1,4 +1,7 @@
-import { Card, Col, Row, Tab, Tabs } from "react-bootstrap";
+import { Button, Card, Col, Row, Tab, Tabs } from "react-bootstrap";
+import { LuCircleCheckBig } from "react-icons/lu";
+import { CgCloseO } from "react-icons/cg";
+import { MdOutlineRemoveRedEye } from "react-icons/md";
 
 export default function Nav_Section() {
     return (
@@ -6,7 +9,7 @@ export default function Nav_Section() {
         <Card>
             <Tabs defaultActiveKey="Solicitudes" className="rounded-pill mb-3" variant="pills">
                 <Tab eventKey="Solicitudes" title="Solicitudes">
-                    Solicitudes de Registro pendientes
+                    <span className="p-3 fw-bold btn-hover-danger">Solicitudes de Registro pendientes</span>
                     <Card className="m-3">
                         <Card.Body>
                             <Card.Title className="fs-6 fw-bold">El Dorado Gourmet</Card.Title>
@@ -34,6 +37,16 @@ export default function Nav_Section() {
                                     <p className="m-0 text-left">Horario propuesto: 11:00 AM - 11:00 PM</p>
                                 </Card.Text>
                                 </Col>
+                            </Row>
+                            <Row className="mt-3">
+                                <hr className="m-0 p-0"/>
+                            </Row>
+                            <Row className="p-0">
+                                <div className="d-flex gap-2">
+                                    <Button variant="success" size="sm" className="fw-bold d-flex align-items-center gap-2"><LuCircleCheckBig size={18} /> Aceptar</Button>
+                                    <Button variant="danger" size="sm" className="fw-bold d-flex align-items-center gap-2 bg-white text-danger"><CgCloseO size={18}/>Rechazar</Button>
+                                    <Button variant="secondary" size="sm" className="fw-bold d-flex align-items-center gap-2 bg-white text-dark"><MdOutlineRemoveRedEye size={18}/>Ver documentos</Button>
+                                </div>
                             </Row>
                         </Card.Body>
                     </Card>
