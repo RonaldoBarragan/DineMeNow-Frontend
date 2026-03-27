292eff7ce75db49d41e0daad674027589023573f
Juan <juancardenas083@gmail.com>
2026-03-26 20:21:09 -0500

    FIx style

--- src/pages/Vista-Restaurante.jsx
+++ src/pages/Vista-Restaurante.jsx
@@ -1,15 +1,15 @@
 import { Card, Container } from "react-bootstrap";
 import Tittle from "../components/comVista-Restaurante/Tittle";
 import Stat from "../components/comVista-Restaurante/Estadisticas";
-import styles from '../components/comVista-Restaurante/style.module.css';
+import '../components/comVista-Restaurante/style.css';
 import NavSecciones from "../components/comVista-Restaurante/Nav-Secciones";
 
 export default function Vista_Restaurante() {
     return (
         <>
-        <Container className={styles["margen-provi container-principal-propio"]}>
+        <Container className="margen-provi container-principal-propio">
             <Tittle />
-            <Stat className={`${styles.clase1} ${otrosStyles.clase2}`}/>
+            <Stat />
             <Card className="p-3">
                 <NavSecciones />
             </Card>
