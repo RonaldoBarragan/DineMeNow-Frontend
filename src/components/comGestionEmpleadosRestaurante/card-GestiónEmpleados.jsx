7812a6873c1869449436b23914c92e0191109014
Lore <monteroluz979@gmail.com>
2026-06-16 15:40:34 -0500

    falta añadir emple y estilos

--- src/components/comGestionEmpleadosRestaurante/card-GestiónEmpleados.jsx
+++ src/components/comGestionEmpleadosRestaurante/card-GestiónEmpleados.jsx
@@ -1,12 +1,15 @@
 import './card-GestiónEmpleados.css'
-import { Card, Container, Row, Col, Tabs, Tab } from "react-bootstrap";
+import { Badge, Card, Container, Row, Col, Tabs, Tab } from "react-bootstrap";
 import { LuBuilding } from "react-icons/lu";
+import { FiEdit } from "react-icons/fi";
+import { FiTrash2 } from "react-icons/fi";
 function CardGestionEmpleados(){
     return (
         <>
         <Container className="gestionempleados">
         <Card className="card-gestionempleados">
           <Card.Body>
+            {/*TITULO*/}
             <LuBuilding  className="icono"/>
             <div>
             <Card.Title className="titulo">Gestión de Empleados</Card.Title>
@@ -14,6 +17,7 @@ function CardGestionEmpleados(){
               Administra el equipo de tu restaurante
             </Card.Text>
             </div>
+            {/*TABS BUTTON*/}
             <Tabs defaultActiveKey="Gestion"  className="mb-3 fondo-tab rounded-pill" variant="pills" fill>
                 <Tab eventKey="Gestion" title="Gestión de Empleados" >
                      
@@ -22,6 +26,7 @@ function CardGestionEmpleados(){
                     
                 </Tab>
             </Tabs>
+            {/*CARDS*/}
             <Row>
                 <Col md={4}>
                     <Card>
@@ -42,7 +47,98 @@ function CardGestionEmpleados(){
                     </Card>
                 </Col>
             </Row>
-
+            {/* LISTA*/}
+            <table className='tabla'>
+                {/*CABECERA DE LA TABLA ID-NOMBRE-CRAGO ETC*/}
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre del Empleado</th>
+                        <th>Cargo</th>
+                        <th>Teléfono</th>
+                        <th>Estado</th>
+                        <th>Acciones</th>
+                    </tr>
+                </thead>
+                {/*contenido de datos*/}
+                <tbody>
+                    <tr>
+                        <td>#001</td>
+                        <td>Carlos Alberto Rodríguez Pérez</td>
+                        <td><Badge bg="secondary">Mesero</Badge></td>
+                        <td>3201234567</td>
+                        <td><Badge bg="secondary">Activo</Badge></td>
+                        <td>
+                            <button className='btn-accion editar'>
+                                <FiEdit />
+                            </button>
+                            <button className='btn-accion eliminar'>
+                                <FiTrash2 />
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>#002</td>
+                        <td>Ana María González López</td>
+                        <td><Badge bg="secondary">Cocina</Badge></td>
+                        <td>3159876543</td>
+                        <td><Badge bg="secondary">Activo</Badge></td>
+                        <td>
+                            <button className='btn-accion editar'>
+                                <FiEdit />
+                            </button>
+                            <button className='btn-accion eliminar'>
+                                <FiTrash2 />
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>#003</td>
+                        <td>Luis Fernando Martínez Silva</td>
+                        <td><Badge bg="secondary">Mesero</Badge></td>
+                        <td>3159876543	</td>
+                        <td><Badge bg="secondary">Vacaciones</Badge></td>
+                        <td>
+                            <button className='btn-accion editar'>
+                                <FiEdit />
+                            </button>
+                            <button className='btn-accion eliminar'>
+                                <FiTrash2 />
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>#004</td>
+                        <td>Patricia Ramírez Castro</td>
+                        <td><Badge bg="secondary">Cocina</Badge></td>
+                        <td>3102345678</td>
+                        <td><Badge bg="secondary">Activo</Badge></td>
+                        <td>
+                            <button className='btn-accion editar'>
+                                <FiEdit />
+                            </button>
+                            <button className='btn-accion eliminar'>
+                                <FiTrash2 />
+                            </button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>#005</td>
+                        <td>Jorge Andrés Sánchez Mora</td>
+                        <td><Badge bg="secondary">Mesero</Badge></td>
+                        <td>3134567890	</td>
+                        <td><Badge bg="secondary">Inactivo</Badge></td>
+                        <td>
+                            <button className='btn-accion editar'>
+                                <FiEdit />
+                            </button>
+                            <button className='btn-accion eliminar'>
+                                <FiTrash2 />
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
 
           </Card.Body>
         </Card>
