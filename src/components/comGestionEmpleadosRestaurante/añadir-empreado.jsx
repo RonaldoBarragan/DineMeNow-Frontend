ef854d55b47b757b8c10840268fd522befe92861
lore <monteroluz979@gmail.com>
2026-06-24 20:56:35 -0500

    falta mas estilos + los botones de editar

--- src/components/comGestionEmpleadosRestaurante/añadir-empreado.jsx
+++ src/components/comGestionEmpleadosRestaurante/añadir-empreado.jsx
@@ -1,43 +1,91 @@
 import { LiaEbay } from 'react-icons/lia';
 import './añadir-empreado.css'
+import {Card, Row, Col,Container, Button, Modal, Form, FormGroup } from 'react-bootstrap'; 
 import { LuUserRoundPlus } from "react-icons/lu";
 import { LuUser } from "react-icons/lu";
+import { CiMail } from "react-icons/ci";
+import { FiPhone } from "react-icons/fi";
+import { FiBriefcase } from "react-icons/fi";
 function Añadirempreado (){
 return (
         <>
-        <div className="card-Añadir">
-            <p className='registro-empleado'> <LuUserRoundPlus /> Registro de Nuevo Empleado</p>
-            <p className='datos-empre'> <LuUser /> Datos Personales</p>
-
-            <label>Primer Nombre *</label>
-            <input type='text' placeholder='Ej:Carlos'></input>
-
-            <label>Segundo Nombre</label>        
-            <input type='text' placeholder='Ej:Alberto'></input>
-
-            <label>Primer Apellido *</label>
-            <input type='text' placeholder=''></input>
-
-            <label>Segundo Apellido</label>
-            <input type='text' placeholder='Ej:Perez'></input>
-
-            <label>Correo Electronico *</label>
-            <input type='email' placeholder='ejemplo@correo.com'></input>
-
-            <label>Telefono *</label>
-            <input type='tel' placeholder='3201234567'></input>
-
-            <p className='datos-empre'>Datos Laborales</p>
-
-            <label>Cargo/Puesto *</label>
-            <select>
-                <option>Seleccione un cargo</option>
-                <option>Mesero</option>
-                <option>Cocina</option>
-                <option>Cajero</option>
-                <option>Supervisor</option>
-            </select>
-        </div>
+        <Card className="card-Añadir">
+            <p className='registro-empleado'> 
+                <LuUserRoundPlus /> 
+                Registro de Nuevo Empleado
+            </p>
+            <p className='datos-empre'> 
+                <LuUser /> 
+                Datos Personales
+            </p>
+            <Form>
+                <Row>
+                    <Col md={6}>
+                        <Form.Group>
+                            <Form.Label>Primer Nombre *</Form.Label>
+                            <Form.Control type='text' placeholder='Ej:Carlos'/>
+                        </Form.Group>
+                    </Col>
+                    <Col md={6}>
+                        <Form.Group>
+                            <Form.Label>Segundo Nombre</Form.Label>
+                            <Form.Control type='text' placeholder='Ej:Alberto'/>                        
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={6}>
+                        <Form.Group>
+                            <Form.Label>Primer Apellido *</Form.Label>
+                            <Form.Control type='text' placeholder='Ej: Rodriguez'/>
+                        </Form.Group>
+                    </Col>
+                    <Col md={6}>
+                        <Form.Group>
+                            <Form.Label>Segundo Apellido</Form.Label>
+                            <Form.Control type='text' placeholder='Ej:Perez'/>
+                        </Form.Group>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col md={6}>
+                    <Form.Group>
+                        <Form.Label> <CiMail /> Correo Electronico *</Form.Label>
+                        <Form.Control type='email' placeholder='ejemplo@correo.com'/>
+                    </Form.Group>
+                    </Col>
+                    <Col md={6}>
+                    <Form.Group>
+                        <Form.Label> <FiPhone /> Telefono *</Form.Label>
+                        <Form.Control  />
+                    </Form.Group>
+                    </Col>
+                </Row>
+                <p className='datos-empre'><FiBriefcase /> Datos Laborales</p>
+                <Row>
+                    <Col>
+                        <Form.Group>
+                            <Form.Label>Cargo/Puesto *</Form.Label>
+                            <Form.Select>
+                                <option disabled>Seleccione un cargo</option>
+                                <option>Mesero</option>
+                                <option>Cocina</option>
+                                <option>Cajero</option>
+                                <option>Supervisor</option>
+                            </Form.Select>
+                        </Form.Group>
+                    </Col>
+                </Row>
+            </Form>
+           <Card className='verificacion'>
+            <p className='desc'>Para completar el registro, se enviará un código de verificación al correo del empleado.</p>
+            <Button><CiMail /> Enviar Codigo de Verificacion</Button>
+           </Card>
+            <div className="d-flex gap-2 mt-3">
+                <Button variant="secondary">Cancelar</Button>
+                <Button variant="primary">Confirmar Registro</Button>
+            </div>
+        </Card>
         
       </>
     )
