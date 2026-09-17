c8a93cd1dec561ebd7dae5d4e85d8cf21931563c
Juan <juancardenas083@gmail.com>
2026-09-17 10:38:16 -0500

    Merge branch 'main' into fix

--- src/pages/Cambiarcontrasena.jsx
+++ src/pages/Cambiarcontrasena.jsx
@@ -1,16 +1,24 @@
 import React from "react";
+import { useLocation, Navigate } from "react-router-dom";
 import BotonCancelar from "../components/common/botonCancelar";
 import ImgLogoGlobal from "../components/common/imgLogo";
 import CardCambiar from "../components/comRecuperarContrasena/CardCambioContrasena";
 
 const CambiarContrasena = () => {
+  const location = useLocation();
+  const resetToken = location.state?.resetToken;
+
+  if (!resetToken) {
+    return <Navigate to="/recuperarcontrasena" replace />;
+  }
+
   return (
     <>
       <BotonCancelar />
       <div style={{ paddingTop: "120px", textAlign: "center" }}>
         <ImgLogoGlobal />
       </div>
-      <CardCambiar />
+      <CardCambiar resetToken={resetToken} />
     </>
   );
 };
